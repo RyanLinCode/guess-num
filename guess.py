fdf2f3de1d0c871ab1c18e1cd109e30801e6f33b
RyanLin <Willie003652@gmail.com>
2021-04-09 15:03:21 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,8 +1,12 @@
 #猜數字 要告訴他 比答案大/小
 
 import random
+start = input('請決定隨機數字範圍開始值')
+end = input('請決定隨機數字範圍結束值')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 
 count = 0
 
